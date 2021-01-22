91d69ae0854a6344e4bade7647de810584d5a0fe
Tudtude <tudtude@gmail.com>
2021-01-22 20:44:50 +0700

    Update raspberry4ha.sh

--- raspberry4ha.sh
+++ raspberry4ha.sh
@@ -8,6 +8,7 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
+echo "----------------- start -----------------------"
 sudo apt install docker-ce -y
 sudo usermod -aG docker ${USER}
 
