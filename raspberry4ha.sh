0c0524792f2a6fcf0abebf3f951ceb48e131fa00
tudtude <tudtude@gmail.com>
2021-01-22 12:42:46 +0000

    first update

---
+++ raspberry4ha.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "------------------ Update --------------------"
+sudo apt update
+
+echo "------------------ Install docker ------------"
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install docker-ce -y
+sudo usermod -aG docker ${USER}
+
+echo "-------------------- Install jq -------------"
+sudo apt install jq -y
